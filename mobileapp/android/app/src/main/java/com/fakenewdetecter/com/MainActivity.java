25c2cc0d66791be1c106a75951eda766be8563ae
Parthiban <parthiban358gu@gmail.com>
2025-11-15 21:03:58 +0530

    main

---
+++ mobileapp/android/app/src/main/java/com/fakenewdetecter/com/MainActivity.java
@@ -0,0 +1,5 @@
+package com.fakenewdetecter.com;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ mobileapp/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.fakenewdetecter.com',
+  appName: 'com.parthiban.app',
+  webDir: 'capacitor.config.json.'
+};
+
+export default config;
