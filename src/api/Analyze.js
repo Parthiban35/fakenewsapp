71c9265d9ff92da37a37aac3582126db0ff77827
Parthiban <parthiban358gu@gmail.com>
2025-11-15 21:43:22 +0530

    main

--- src/api/Analyze.js
+++ src/api/Analyze.js
@@ -1,7 +1,23 @@
-export async function analyzeFakeNews(text) {
-  // TODO: Connect real AI API here
-  // For now → random fake score
-  const fakeScore = Math.floor(Math.random() * 100);
+// export async function analyzeFakeNews(text) {
+//   // TODO: Connect real AI API here
+//   // For now → random fake score
+//   const fakeScore = Math.floor(Math.random() * 100);
+
+//   return {
+//     score: fakeScore,
+//     message:
+//       fakeScore > 50
+//         ? "This news looks suspicious ❗"
+//         : "This news seems credible ✔"
+//   };
+// }
+export function analyzeFakeNews(text) {
+  let hash = 0;
+  for (let i = 0; i < text.length; i++) {
+    hash = text.charCodeAt(i) + ((hash << 5) - hash);
+  }
+
+  const fakeScore = Math.abs(hash % 100);
 
   return {
     score: fakeScore,
